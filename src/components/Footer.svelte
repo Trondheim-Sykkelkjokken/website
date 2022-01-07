4bb2663cc216390826163a5585794cb57748fece
Sverre Johann Bjørke <sverrejb@gmail.com>
2022-01-07 23:26:54 +0100

    add files

---
+++ src/components/Footer.svelte
@@ -0,0 +1,42 @@
+<footer>
+	<div id="footer-banner">
+		<div class="flex-container">
+			<div class="footer-item">Stammen kafe og bar</div>
+			<div class="footer-item">Trondheim Sykkelkjøkken</div>
+			<div class="footer-item">Social media links here</div>
+		</div>
+	</div>
+</footer>
+
+<style>
+	footer {
+		padding-top: 1rem;
+		background-color: #efeb92;
+		min-height: 3rem;
+	}
+
+	#footer-banner {
+		background-color: #efa170;
+		width: 100%;
+	}
+
+	.flex-container {
+		max-width: 1000px;
+		margin: 0 auto;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+	}
+
+	.footer-item {
+		flex-grow: 1;
+	}
+
+	.footer-item:nth-child(2) {
+		text-align: center;
+	}
+
+	.footer-item:last-child {
+		text-align: right;
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,57 @@
+<header>
+	<div id="nav-wrapper">
+		<nav>
+			<a href="/"><img alt="" src="/logo.svg" /></a>
+			<a href="/about">About</a>
+			<a href="/blog">Blog</a>
+			<a href="/events">Events</a>
+			<a href="/resources">Resources</a>
+			<a href="/getinvolved">Get involved</a>
+			<a href="/contact">Contact</a>
+		</nav>
+	</div>
+	<div class="info" />
+</header>
+
+<style>
+	#nav-wrapper {
+		background-color: #efeb92;
+		padding-bottom: 1rem;
+	}
+
+	nav {
+		max-width: 1000px;
+		margin: -5rem auto 0 auto;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
+
+	a,
+	a:visited,
+	a:hover,
+	a:active {
+		color: #676a3e;
+		text-decoration: none;
+		text-transform: uppercase;
+		flex-grow: 1;
+		font-size: 1.2rem;
+		align-self: baseline;
+	}
+
+	a:first-child {
+		position: relative;
+		top: 5rem;
+	}
+
+	.info {
+		height: 2rem;
+		background-color: #fcf6d2;
+	}
+
+	img {
+		display: inline;
+		width: 10rem;
+		left: 5rem;
+	}
+</style>
