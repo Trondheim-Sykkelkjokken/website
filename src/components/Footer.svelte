b7e73f2c87264e6991896d557f65ed636555aeea
Sverre Johann Bjørke <sverrejb@gmail.com>
2022-01-09 19:47:39 +0100

    footer stuff

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,23 +1,56 @@
+<script context="module">
+	import Icon from 'svelte-icons-pack/Icon.svelte';
+	import AiOutlineMail from 'svelte-icons-pack/ai/AiOutlineMail';
+	import AiOutlineFacebook from 'svelte-icons-pack/ai/AiOutlineFacebook';
+	import AiOutlineInstagram from 'svelte-icons-pack/ai/AiOutlineInstagram';
+	import AiOutlineGithub from 'svelte-icons-pack/ai/AiOutlineGithub';
+
+	const iconProps = { size: '2.5rem', color: 'whitesmoke' };
+</script>
+
 <footer>
 	<div id="footer-banner">
 		<div class="flex-container">
-			<div class="footer-item">Stammen kafe og bar</div>
+			<div class="footer-item">
+				<a href="https://g.page/stammencafe?share"
+					>Stammen Café og Bar, <br /> Kongens gate 55, <br /> Trondheim, Norway</a
+				>
+			</div>
 			<div class="footer-item">Trondheim Sykkelkjøkken</div>
-			<div class="footer-item">Social media links here</div>
+			<div class="footer-item">
+				<a href="mailto:trondheimsykkelkjokken@gmail.com"
+					><Icon {...iconProps} src={AiOutlineMail} /></a
+				>
+				<a href="https://www.facebook.com/trondheimsykkelkjokken"
+					><Icon {...iconProps} src={AiOutlineFacebook} /></a
+				>
+				<a href="https://www.instagram.com/trondheimsykkelkjokken/"
+					><Icon {...iconProps} src={AiOutlineInstagram} /></a
+				>
+				<a href="https://github.com/Trondheim-Sykkelkjokken"
+					><Icon {...iconProps} src={AiOutlineGithub} /></a
+				>
+			</div>
 		</div>
 	</div>
 </footer>
 
 <style>
+	a {
+		color: whitesmoke;
+		text-decoration: none;
+	}
+
 	footer {
 		padding-top: 1rem;
 		background-color: #efeb92;
-		min-height: 3rem;
+		min-height: 6rem;
 	}
 
 	#footer-banner {
 		background-color: #efa170;
 		width: 100%;
+		padding: 1rem 0;
 	}
 
 	.flex-container {
@@ -30,6 +63,8 @@
 
 	.footer-item {
 		flex-grow: 1;
+		color: whitesmoke;
+		align-self: center;
 	}
 
 	.footer-item:nth-child(2) {
