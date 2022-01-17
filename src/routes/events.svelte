65bd07ecf3c1aaa17860e1bdb7335b9439dd21c3
Sverre Johann Bjørke <sverrejb@gmail.com>
2022-01-17 12:33:42 +0100

    remove iframe

--- src/routes/events.svelte
+++ src/routes/events.svelte
@@ -1,13 +1,1 @@
 <h1>Events</h1>
-
-<iframe
-	title="Events"
-	src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftrondheimsykkelkjokken&tabs=events&width=500&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-	width="500"
-	height="1500"
-	style="border:none;overflow:hidden"
-	scrolling="no"
-	frameborder="0"
-	allowfullscreen
-	allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-/>
