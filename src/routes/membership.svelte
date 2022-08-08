7ec7a0391e192e501e416d35929e737ceec1d0ed
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2022-08-08 10:54:13 +0200

    updated membership link

--- src/routes/membership.svelte
+++ src/routes/membership.svelte
@@ -2,6 +2,6 @@
 
 <h2>Become a member:</h2>
 <a
-	href="https://l.facebook.com/l.php?u=https%3A%2F%2Fdocs.google.com%2Fforms%2Fd%2Fe%2F1FAIpQLSfMpyJCKUkfD2hfunoOwGSJUcvM29RowHGlPGynwzdE3DaUMw%2Fviewform%3Fusp%3Dsf_link%26fbclid%3DIwAR2E6gHdxUW9ue9NJNmUWGXw3RWmN8D7LdY0IrMgyLhtHMCJLAjENpzPewE&h=AT30CrSZEnOVfOi3n8YRkOIS1EKPRUm_q4kJsQh5Si5KuSwHME0F6167WR0oAPUJy19t9P4j_s2jHhg9DVzjkdaEkuqRtswQAuBs-0LWs0CQPwOcw5j40JVfHYfFLvT21NkJRYnIx2WuX6tUSWaKXQ"
+	href="https://docs.google.com/forms/d/e/1FAIpQLSfMpyJCKUkfD2hfunoOwGSJUcvM29RowHGlPGynwzdE3DaUMw/viewform"
 	>Fill out our membership form</a
 >
