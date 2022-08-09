a2a79f6481ddc25f7f25ec0b96bcb653370ec291
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2022-08-09 23:13:04 +0200

    embed google form

--- src/routes/membership.svelte
+++ src/routes/membership.svelte
@@ -1,7 +1,9 @@
-<h1>Membership</h1>
-
-<h2>Become a member:</h2>
-<a
-	href="https://docs.google.com/forms/d/e/1FAIpQLSfMpyJCKUkfD2hfunoOwGSJUcvM29RowHGlPGynwzdE3DaUMw/viewform"
-	>Fill out our membership form</a
+<iframe
+	id="foo"
+	title="Membership form"
+	src="https://docs.google.com/forms/d/e/1FAIpQLSfMpyJCKUkfD2hfunoOwGSJUcvM29RowHGlPGynwzdE3DaUMw/viewform?embedded=true"
+	style="overflow:hidden;height:100%;width:100%"
+	frameborder="0"
+	marginheight="0"
+	marginwidth="0">Loading…</iframe
 >
