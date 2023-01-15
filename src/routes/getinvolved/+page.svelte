17ff923f562b56804be153c7fe0f6792a0a1d4f4
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2023-01-15 23:07:54 +0100

    text for get involved page

--- src/routes/getinvolved/+page.svelte
+++ src/routes/getinvolved/+page.svelte
@@ -1,1 +1,12 @@
 <h1>Get involved!</h1>
+
+<p>We are always looking for people to contribute to the bike kitchen! If you:</p>
+<ul>
+	<li>Want to become a volunteer and help out</li>
+	<li>Want to hold a repair course</li>
+	<li>Want to hold a talk about biking</li>
+	<li>Want to show off a custom bike</li>
+	<li>Want to make Trondheim a better place for biking</li>
+	<li>Want to make new friends and learn something</li>
+</ul>
+<span>then please contact us!</span>
