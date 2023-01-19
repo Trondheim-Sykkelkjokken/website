d815766bd918c46fc161bc1f0ca91b6f755feeb7
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2023-01-19 11:17:39 +0100

    fuck facebook

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,17 +1,2 @@
 <h1>Events</h1>
 <p>No upcoming events. Stay tuned!</p>
-<div
-	class="fb-page"
-	data-href="https://www.facebook.com/trondheimsykkelkjokken"
-	data-tabs="events"
-	data-width="500"
-	data-height="600"
-	data-small-header="true"
-	data-adapt-container-width="true"
-	data-hide-cover="true"
-	data-show-facepile="true"
->
-	<blockquote cite="https://www.facebook.com/trondheimsykkelkjokken" class="fb-xfbml-parse-ignore">
-		<a href="https://www.facebook.com/trondheimsykkelkjokken">Trondheim Sykkelkjøkken</a>
-	</blockquote>
-</div>
