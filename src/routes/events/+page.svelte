33f51b892f4b34d90e4cdbd7f9ffa932a41717e4
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2023-01-19 10:19:33 +0100

    test facebook page plugin

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,2 +1,17 @@
 <h1>Events</h1>
 <p>No upcoming events. Stay tuned!</p>
+<div
+	class="fb-page"
+	data-href="https://www.facebook.com/trondheimsykkelkjokken"
+	data-tabs="events"
+	data-width="500"
+	data-height=""
+	data-small-header="false"
+	data-adapt-container-width="true"
+	data-hide-cover="false"
+	data-show-facepile="true"
+>
+	<blockquote cite="https://www.facebook.com/trondheimsykkelkjokken" class="fb-xfbml-parse-ignore">
+		<a href="https://www.facebook.com/trondheimsykkelkjokken">Trondheim Sykkelkjøkken</a>
+	</blockquote>
+</div>
