f725392cc3717ef0d30212917e370f95afd05cbe
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2023-01-29 20:21:36 +0100

    update events page

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Events</h1>
-<p>No upcoming events. Stay tuned!</p>
+<p>
+	Currently, our events are only listed on our <a
+		href="https://www.facebook.com/trondheimsykkelkjokken/events">facebook page</a
+	>. We are working on posting them here as well.
+</p>
