dca7cb6dfdacae505bb9b83aec02a97a99c5ecd5
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2023-08-01 16:07:49 +0200

    add tnc component

---
+++ src/routes/membership/tnc.svelte
@@ -0,0 +1,56 @@
+<script>
+	import Icon from 'svelte-icons-pack/Icon.svelte';
+	import AiOutlineCreditCard from 'svelte-icons-pack/ai/AiOutlineCreditCard';
+</script>
+
+<div class="tandc">
+	<h2>Terms and conditions</h2>
+	<p>
+		Paragraph 1: In accordance with the provisions set forth in the aforementioned agreement, the
+		undersigned party shall hereby indemnify and hold harmless all parties involved from any and all
+		claims, damages, or liabilities arising out of or in connection with the execution of this
+		contract. Furthermore, it is hereby acknowledged that any breach of the terms and conditions
+		outlined herein shall result in the immediate termination of this agreement, without prejudice
+		to any other rights or remedies available to the aggrieved party.
+	</p>
+	<p>
+		Paragraph 2: This document serves as an addendum to the existing legal agreement between the
+		parties hereto. The parties agree to promptly and diligently perform all obligations set forth
+		in said agreement, and furthermore, shall refrain from engaging in any action or conduct that
+		may be deemed in violation of the terms and conditions stipulated herein. Any failure to comply
+		with the provisions contained herein may result in legal action, including but not limited to,
+		the seeking of injunctive relief and the recovery of damages incurred as a direct result of the
+		breach.
+	</p>
+	<p>
+		Paragraph 3: Notwithstanding any provision to the contrary contained herein, all disputes
+		arising out of or relating to this agreement shall be subject to binding arbitration in
+		accordance with the rules and regulations set forth by the American Arbitration Association. The
+		selected arbitrator shall have the authority to
+	</p>
+
+	<p>
+		By becoming a member of Trondheim Bike Kitchen, you commit to the following code of conduct:
+	</p>
+	<ul>
+		<li>Respect people, bicycles and tools at the bike kitchen.</li>
+		<li>Make sure your bike is clean before you bring it inside.</li>
+		<li>Take good care of tools and clean up after yourself.</li>
+		<li>Help others and share your knowledge.</li>
+	</ul>
+	<p>ALL PARTICIPATION IN ACTIVITIES AT THE BIKE KITCHEN IS AT YOUR OWN RISK.</p>
+	<p>
+		If the code of conduct is not respected, you will have to leave the premises and the Bike
+		Kitchen's activities, and you risk losing your membership.
+	</p>
+</div>
+
+<style>
+	.tandc {
+		background-color: whitesmoke;
+		color: black;
+		font-family: monospace;
+		padding: 1rem;
+		margin-bottom: 3rem;
+	}
+</style>
