6ec63491b247073b99c02d6ff44a208db1a30349
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2023-08-01 13:48:07 +0200

    form actions, buttons etc

--- src/routes/membership/+page.server.js
+++ src/routes/membership/+page.server.js
@@ -1,6 +1,12 @@
 /** @type {import('./$types').Actions} */
+
+// @ts-ignore
+const prices = { year: 330, year_reduced: 220, semester: 215, semester_reduced: 150 };
+
+
 export const actions = {
-    default: async (event) => {
+    payWithVipps: async (event) => {
+        console.log("paying with vipps");
         const formData = await event.request.formData();
         const name = formData.get("name");
         const email = formData.get("email");
@@ -8,5 +14,16 @@ export const actions = {
         console.log(formData);
 
         return { success: true, error: false };
+    },
+    payWithCard: async (event) => {
+        console.log("paying with card")
     }
 };
+
+export function load({ params }) {
+
+    return {
+        prices
+    };
+}
+
--- src/routes/membership/+page.svelte
+++ src/routes/membership/+page.svelte
@@ -1,6 +1,9 @@
 <script>
 	/** @type {import('./$types').ActionData} */
 	export let form;
+	export let data;
+
+	let selectedMembership = 'year';
 </script>
 
 <h1>Become a member!</h1>
@@ -20,20 +23,51 @@
 			<legend>Choose membership type</legend>
 
 			<label
-				><input name="membershipType" type="radio" value="year" required />Full-year membership (1st
-				July 2023 - 30th June 2024)
+				><input
+					name="membershipType"
+					bind:group={selectedMembership}
+					type="radio"
+					value="year"
+					required
+				/>Full-year membership (1stJuly 2023 - 30th June 2024)
 			</label>
 			<label
-				><input name="membershipType" type="radio" value="semester" />Semester membership (1st July
-				2023 - 31th December 2023)
+				><input
+					name="membershipType"
+					bind:group={selectedMembership}
+					type="radio"
+					value="year_reduced"
+					required
+				/>Full-year membership (1stJuly 2023 - 30th June 2024) - Reduced price<sup>*</sup>
 			</label>
-			<label>
-				<input type="checkbox" name="reducedPrice" />
-				Reduced price for students, children, pensioners and unemployed.</label
-			>
+			<label
+				><input
+					name="membershipType"
+					bind:group={selectedMembership}
+					type="radio"
+					value="semester"
+				/>Semester membership (1st July2023 - 31th December 2023)
+			</label>
+			<label
+				><input
+					name="membershipType"
+					bind:group={selectedMembership}
+					type="radio"
+					value="semester_reduced"
+				/>Semester membership (1st July2023 - 31th December 2023) - Reduced Price<sup>*</sup>
+			</label>
+
+			<p>
+				<sup>*</sup> Reduced price is only available for students, pensioners and unemployed persons.
+			</p>
 		</fieldset>
 
-		<input type="submit" value="Submit" />
+		<div class="line" />
+		<p class="total">Price: NOK {data.prices[selectedMembership]}</p>
+		<button class="vipps_button" formaction="?/payWithVipps"
+			><img alt="" src="/vipps_english.svg" /></button
+		>
+		<button class="card_button" formaction="?/payWithCard">Pay with card</button>
 	</form>
 {/if}
 
@@ -49,5 +83,39 @@
 	label {
 		display: block;
 		margin: 0.5rem 0;
+	}
+
+	.line {
+		margin-top: 3rem;
+		width: 100%;
+		border-top: 3px solid white;
+	}
+
+	.total {
+		font-size: 2.5rem;
+	}
+
+	button {
+		display: block;
+		margin: 1rem 0;
+	}
+
+	.vipps_button {
+		background: none;
+		border: none;
+	}
+
+	.card_button {
+		font-size: 1.2rem;
+		background-color: #ddd;
+		border: none;
+		color: black;
+		padding: 10px 20px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		margin: 4px 2px;
+		cursor: pointer;
+		border-radius: 25px;
 	}
 </style>
