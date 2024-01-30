fe790458209c66795edf3b39887ba42f7d4f4a18
Sverre Johann Bjørke <sverrejb@gmail.com>
2024-01-30 20:17:26 +0100

    List some of the events we do

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-	export const prerender = true;
 </script>
 
 <Header />
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -4,3 +4,18 @@
 		href="https://www.facebook.com/trondheimsykkelkjokken/events">Facebook page</a
 	>.
 </p>
+<h2>Some of the things we like to do:</h2>
+<ul>
+	<li>Bike repair Wednesdays</li>
+	<li>Winter tire changing days</li>
+	<li>Easter, christmas, valentines and summer events</li>
+	<li>Bike trips with sleepovers</li>
+	<li>Move screenings</li>
+	<li>Repair courses</li>
+	<li>Bike talks</li>
+	<li>
+		... Maybe YOU can think of something? Let us know, or become an <a href="/volunteer"
+			>volunteer!</a
+		>
+	</li>
+</ul>
