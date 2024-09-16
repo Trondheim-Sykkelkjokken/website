c1a53735f63e649de765404b878b905b357e1831
Sverre Johann Bjørke <sverrejb@gmail.com>
2024-09-16 08:59:50 +0200

    disable prerender of events page ++

--- src/routes/events/+page.server.ts
+++ src/routes/events/+page.server.ts
@@ -1,3 +1,5 @@
+export const prerender = false;
+
 import { EVENTBRITE_API_KEY } from '$env/static/private';
 import type { EventBriteResponse } from '$lib/types/events.type';
 import fixtureEvents from '$lib/fixtures/test-events.json';
@@ -5,16 +7,19 @@ import fixtureEvents from '$lib/fixtures/test-events.json';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-    const response = await fetch('https://www.eventbriteapi.com/v3/organizations/486298958703/events?expand=venue', {
+    const response = await fetch('https://www.eventbriteapi.com/v3/organizations/486298958703/events', {
         headers: {
             'Authorization': `Bearer ${EVENTBRITE_API_KEY}`,
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
         }
     });
-    const data: EventBriteResponse = await response.json();
+    const data = await response.json();
+
+    console.log(data)
 
     if (process.env.NODE_ENV === 'development') {
-        data.events.push(...fixtureEvents);
+        data.events.push(...fixtureEvents as Event[]);
     }
 
     return {
