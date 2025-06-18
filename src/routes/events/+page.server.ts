81c7ac92f902cac4b956de58443860b71c106aa5
Sverre Johann Bjørke <sverrejb@gmail.com>
2025-06-18 14:52:21 +0200

    add live status filter to events request

--- src/routes/events/+page.server.ts
+++ src/routes/events/+page.server.ts
@@ -7,25 +7,23 @@ import type { BikeKitchenEvent, BikeKitchenEventWithVenue, EventBriteResponse, V
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-    const eventResponse = await fetch('https://www.eventbriteapi.com/v3/organizations/486298958703/events', {
+    const eventResponse = await fetch('https://www.eventbriteapi.com/v3/organizations/486298958703/events?status=live', {
         headers: {
             'Authorization': `Bearer ${EVENTBRITE_API_KEY}`,
-            'Content-Type': 'application/json'
         }
     });
 
     const locationResponse = await fetch('https://www.eventbriteapi.com/v3/organizations/486298958703/venues', {
         headers: {
             'Authorization': `Bearer ${EVENTBRITE_API_KEY}`,
-            'Content-Type': 'application/json'
         }
     });
 
     let [eventData, venueData]: [EventBriteResponse, VenueResponse] = await Promise.all([eventResponse.json(), locationResponse.json()]);
 
-    if (process.env.NODE_ENV === 'development') {
-        eventData.events.push(...fixtureEvents as unknown as BikeKitchenEvent[]);
-    }
+    // if (process.env.NODE_ENV === 'development') {
+    //     eventData.events.push(...fixtureEvents as unknown as BikeKitchenEvent[]);
+    // }
 
     const venueMap = new Map(venueData.venues.map(v => [v.id, v]));
 
