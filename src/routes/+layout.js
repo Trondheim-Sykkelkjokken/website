1116eac584308ffdb4a26eed724ec2c4831dc486
Sverre Johann Bjørke <sverrejb@gmail.com>
2025-06-28 13:42:13 +0200

    add debug logging

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -7,6 +7,9 @@ export const prerender = true;
 /** @type {import('@sveltejs/kit').Load} */
 export const load = async ({ url, data }) => {
     const { pathname } = url;
+
+    console.log("locale from server: " + data?.locale);
+
     const locale = data?.locale || 'en';
 
     await loadTranslations(locale, pathname);
