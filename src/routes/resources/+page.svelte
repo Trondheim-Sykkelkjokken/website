4bb82c415ef627400f8e55b2e96075d93e2a9603
Peter Remøy Paulsen <peter.remoy.paulsen@gmail.com>
2025-07-05 17:05:44 +0200

    Fixed some routing and the pay with vipps button for nn an nb

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,15 +1,15 @@
 <script>
+	import { locale } from '$lib/translations';
+	import PageEn from '../../content/pages/resources/resources.en.md';
+	import PageNb from '../../content/pages/resources/resources.nb.md';
+	import PageNn from '../../content/pages/resources/resources.nn.md';
 
-    import { locale } from '$lib/translations';
-    import PageEn from '../../content/pages/resources/resources.en.md';
-    import PageNb from '../../content/pages/resources/resources.nb.md';
-    import PageNn from '../../content/pages/resources/resources.nn.md';
-
-    let currentLocale = 'en';
-    locale.subscribe((value) => {
-        currentLocale = value;
+	let currentLocale = 'en';
+	locale.subscribe((value) => {
+		currentLocale = value;
 	});
-  
-  </script>
-  
-  <svelte:component this={currentLocale === 'nn' ? PageNn : currentLocale === 'nb' ? PageNb : PageEn} />
+</script>
+
+<svelte:component
+	this={currentLocale === 'nn' ? PageNn : currentLocale === 'nb' ? PageNb : PageEn}
+/>
--- src/routes/tools/+page.svelte
+++ src/routes/tools/+page.svelte
@@ -1,14 +1,15 @@
 <script>
+	import { locale } from '$lib/translations';
+	import PageEn from '../../content/pages/tools/tools.en.md';
+	import PageNb from '../../content/pages/tools/tools.nb.md';
+	import PageNn from '../../content/pages/tools/tools.nn.md';
 
-    import { locale } from '$lib/translations';
-    import PageEn from '../../content/pages/tools/tools.en.md';
-    import PageNb from '../../content/pages/tools/tools.nb.md';
-
-    let currentLocale = 'en';
-    locale.subscribe((value) => {
-        currentLocale = value;
-	  });
-  
+	let currentLocale = 'en';
+	locale.subscribe((value) => {
+		currentLocale = value;
+	});
 </script>
-  
-  <svelte:component this={currentLocale === 'nb' ? PageNb : PageEn} />
+
+<svelte:component
+	this={currentLocale === 'nn' ? PageNn : currentLocale === 'nb' ? PageNb : PageEn}
+/>
